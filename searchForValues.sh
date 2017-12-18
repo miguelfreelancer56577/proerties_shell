5e2bf516dcd18cc2d716d13f5072b7da19095e8d
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-12-18 10:11:41 -0600

    I've created a new function to get the position, variable's name and variable's value to arrayswq

--- searchForValues.sh
+++ searchForValues.sh
@@ -5,17 +5,40 @@ arr_var=()
 
 count=0
 
+arr_positionNumber=()
+arr_variableNames=()
+arr_variableValues=()
+
 function getVariables(){
 
  oldIFS=$IFS
 
  IFS=$1
 
+ innerCount=0
+
  for item in $2
   do
+
    echo $item $count
-   arr_var[$count]="$item"
+
+   if [ $innerCount -eq 0 ]
+    then
+
+     arr_var[$count]="$item"
+
+    else
+
+     echo getValue "$item" "$count"
+
+   fi
+
+#   arr_var[$count]="$item"
+
    let count++
+
+   let innerCount++
+
  done
    
  IFS=$oldIFS
@@ -24,16 +47,84 @@ function getVariables(){
 
 }
 
+function getValue(){
 
+ echo get value to change
 
+ local oldIFS=$IFS
+
+ IFS=:
+
+ local positionInArray=0;
+
+ local count=0;
+
+ for item in $1
+ do
+
+  if [ $positionInArray -eq $count ]
+  then
+
+   arr_var[$2]="$item"
+
+   return
+
+  fi
+
+  let count++;
+
+ done
+
+ IFS=$oldIFS
+
+}
+
+function onChange(){
+
+  local array_positionNumber=()
+  local array_variableNames=()
+  local array_variableValues=()
+
+  local count=0;
+
+  #$1 file's name
+  
+  # local saveV="$(cat $1 | grep '##')"
+  # local saveV="$(cat -n example.sh | grep '##' | sed -r 's/\s{3,}//g')"
+  local saveV="$(cat -n example.sh | grep '##' | sed -r "s/\s/_/g" | tr -d '[:space:]' | sed -r 's/_{2,}//g')"  
+
+  local oldIFS=$IFS
+
+  IFS="##"
+
+  for linea in $saveV
+  do
+    if [ ${#linea} -gt 0 ]; then
+        array_positionNumber[$count]="$(echo $linea | cut -d '_' -f 1)"
+        array_variableNames[$count]="$(echo $linea | cut -d '_' -f 2 | cut -d "=" -f 1)"
+        array_variableValues[$count]="$(echo $linea | cut -d '_' -f 2 | cut -d "=" -f 2)"
+        let count++
+    fi
+
+  done
+
+  IFS=$oldIFS
+
+  arr_positionNumber=("${array_positionNumber[*]}")
+  arr_variableNames=("${array_variableNames[*]}")
+  arr_variableValues=("${array_variableValues[*]}")
+
+}
+
+# get variables and values from a property file.
 
 while read line; do
+
  getVariables "=" $line
+
 done < properties.proerties_shell.conf
 
-echo '***************'
+# get position, variable´s name and  variable's value to arrays
 
-echo ${#arr_var[*]}
-
-echo ${arr_var[*]}
+onChange "example.sh"
 
